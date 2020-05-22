bbe68f5e8ff2334f9fb4d4a30cb92a1acea215b2
Jesus Lopez <jesus.lopez@xing.com>
2020-05-22 11:21:28 +0200

    Add some input validations

--- lib/xsimulator/input/input.rb
+++ lib/xsimulator/input/input.rb
@@ -70,6 +70,10 @@ class Input
   def process_input_area(area_data)
     area_data = area_data.map(&:to_i) # TODO: Strings to int cast to 0
 
+    unless valid_area?(area_data)
+      raise StandardError, 'Invalid data area' # TODO: Custom exceptions class
+    end
+
     puts "Area: #{area_data.join(',')}"
 
     @area_coordinate = Coordinate.new(area_data.first, area_data.last)
@@ -79,6 +83,10 @@ class Input
     position_data[0] = position_data.first.to_i
     position_data[1] = position_data[1].to_i
 
+    unless valid_rover?(position_data, moves_data)
+      raise StandardError, 'Invalid data rover' # TODO: Custom exceptions class
+    end
+
     puts 'Rover data'
     puts position_data.join(',')
     puts moves_data.join(',')
@@ -86,5 +94,49 @@ class Input
     rover_input = RoverInput.new(position_data, moves_data)
 
     @rovers.append(rover_input)
+  end
+
+  def valid_area?(data)
+    is_valid_length = data.length == 2
+    is_valid_position = data.first.is_a?(Integer) && data[1].is_a?(Integer)
+
+    result = is_valid_length && is_valid_position
+
+    unless result
+      raise StandardError, 'Invalid area provided' # TODO: Custom exceptions class
+    end
+
+    result
+  end
+
+  def valid_rover?(position_data, moves_data)
+    valid_rover_positions?(position_data) && valid_rover_moves?(moves_data)
+  end
+
+  def valid_rover_positions?(position_data)
+    is_valid_length = position_data.length == 3
+    is_valid_position = position_data.first.is_a?(Integer) && position_data[1].is_a?(Integer)
+    is_valid_orientation = position_data[2].is_a?(String) # TODO: Verify is one of the valid ["N", "S", "E", "W"]
+
+    result = is_valid_length && is_valid_position && is_valid_orientation
+
+    unless result
+      raise StandardError, 'Invalid rover positions provided' # TODO: Custom exceptions class
+    end
+
+    result
+  end
+
+  def valid_rover_moves?(moves_data)
+    result = moves_data.all? do |move|
+      move.is_a?(String)
+      # TODO: Verify if is a valid move ["M", "L", "R"]
+    end
+
+    unless result # TODO: Return unless is valid here?
+      raise StandardError, 'Invalid rover moves provided' # TODO: Custom exceptions class
+    end
+
+    result
   end
 end
