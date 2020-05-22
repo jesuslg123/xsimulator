32ece1e57e748946edf7a5bcef2a07728d515b7b
Jesus Lopez <jesus.lopez@xing.com>
2020-05-22 09:29:57 +0200

    Add support to read cli run argument

--- lib/xsimulator.rb
+++ lib/xsimulator.rb
@@ -4,6 +4,17 @@ require 'byebug'
 
 def main
   puts 'Initializing....'
+
+  input_file_path = process_path_from_args
+  puts input_file_path
+end
+
+def process_path_from_args
+  if ARGV.empty?
+    raise StandardError, 'Missing input file parameter' # TODO: Custom exceptions class
+  end
+
+  ARGV.first
 end
 
 main
